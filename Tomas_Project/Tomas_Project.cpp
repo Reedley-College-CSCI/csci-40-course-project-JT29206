9c96cee9b6dd36000fbd77fdbbbc26de99713ae5
jaslene.thomas4@gmail.com <jaslene.thomas4@gmail.com>
2026-04-21 17:51:29 -0700

    Created struct and let users enter information about their car

---
+++ Tomas_Project/Tomas_Project.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Automotive {
+	string make;
+	string model;
+	int year;
+
+};
+
+
+int main() {
+	string make;
+	string model;
+	int year;
+	cout << "Enter the make of your car: " << endl;
+	cin >> make;
+	cout << "Enter the model of your car: " << endl;
+	cin >> model;
+	cout << "Enter the year of your car: " << endl;
+	cin >> year;
+	cout << "You have inputted a " << make << " " << model << " " << year << endl;
+
+	return 0;
+}
